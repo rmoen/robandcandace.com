e68104de3fbe340a7c746945f3492366670f9152
Rob Moen <rob.moen@gmail.com>
2016-04-04 15:55:19 -0700

    Split out sectoins, added scroll

--- client/routes.js
+++ client/routes.js
@@ -1,9 +1,17 @@
 angular.module('rsvp').config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
 	$locationProvider.html5Mode(true);
 	$stateProvider
-		.state('rsvpForm', {
+		.state('rsvpDetails', {
 			url: '/',
+			templateUrl: 'client/rsvp/rsvp-details/details.html'
+		})
+		.state('rsvpForm', {
+			url: '/rsvp',
 			template: '<rsvp-form></rsvp-form>'
+		})
+		.state('rsvpMap', {
+			url: '/map',
+			templateUrl: 'client/rsvp/rsvp-map/map.html'
 		})
 		.state('rsvpList', {
 			url: '/rsvp-list',
